9759439beec3160ec9cfdde7b14a60a101d18634
Madalyn Marabella <mmarabella@utexas.edu>
2019-03-16 18:22:02 -0500

    adds routing functions to main.py

--- main.py
+++ main.py
@@ -1,36 +1,26 @@
-#-----------------------------------------
+#----------------------------------------
 # app2.py
 # creating first flask application
 #-----------------------------------------
 from flask import Flask, render_template
 
-
-
-
 app = Flask(__name__)
-
-
-
-
 
 @app.route('/')
 def index():
+	return render_template('index.html')
+
+@app.route('/games/')
+def games():
 	return render_template('games.html')
 
+@app.route('/genres/')
+def genres():
+	return render_template('genres.html')
 
-
-
-
-'''
-@app.route('/book/')
-def book():
-	return render_template('book.html')
-'''
-
-
-
-
-
+@app.route('/companies/')
+def companies():
+	return render_template('companies.html')
 
 if __name__ == "__main__":
-	app.run()
+  app.run()
