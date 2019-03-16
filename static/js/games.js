da589067a07ec7fc6aecbf1b28d53fa704399101
Alejandro Palacios <alex16palashus@gmail.com>
2019-03-16 15:47:09 -0500

    Added games to game page

---
+++ static/js/games.js
@@ -0,0 +1,49 @@
+
+
+
+// Hover Categories
+
+function displayDescription(event) {
+
+	var projectHolder = event.currentTarget.childNodes[1];
+	var description = projectHolder.childNodes[3];
+	description.style.opacity = "1";
+
+
+
+}
+
+function hideDescription(event) {
+
+	var projectHolder = event.currentTarget.childNodes[1];
+	var description = projectHolder.childNodes[3];
+	description.style.opacity = "0";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
