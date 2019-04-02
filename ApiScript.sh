1da41d06f8270e9d0e9fccce1a02d2b02f5b2aac
Madalyn Marabella <mmarabella@utexas.edu>
2019-04-01 21:33:26 -0500

    generates games and genres json files

--- ApiScript.sh
+++ ApiScript.sh
@@ -1,4 +1,6 @@
 rm -f companies.json
+rm -f games.json
+rm -f genres.json
 
 printf "{\n\"Companies\": [\n" > companies.json
 
@@ -14,12 +16,26 @@ where developed != null & logo != null & description != null;' \
 
 printf "]\n}" >> companies.json
 
-#curl -H 'user-key: 401df87a02a8f4c13842a135bad415a4' \
-#-d 'fields name; limit 5; \
-#where developed != null & logo != null & description != null;' \
-#'https://api-v3.igdb.com/companies/' | jq '.[]'
 
-#curl -H 'user-key: 401df87a02a8f4c13842a135bad415a4' \
-#-d 'fields name; offset 4; limit 5; \
-#where developed != null & logo != null & description != null;' \
-#'https://api-v3.igdb.com/companies/' | jq '.[]'
+printf "{\n\"Games\": [\n" > games.json
+
+curl -H 'user-key: 401df87a02a8f4c13842a135bad415a4' \
+-d 'fields name, rating, genres, involved_companies, url, summary; limit 50; \
+where developed != null & logo != null & description != null;' \
+'https://api-v3.igdb.com/games/' | jq '.[]' >> games.json
+
+curl -H 'user-key: 401df87a02a8f4c13842a135bad415a4' \
+-d 'fields name, rating, genres, involved_companies, url, summary; offset 50; limit 50; \
+where rating != null & genres != null & summary != null & involved_companies != null;' \
+'https://api-v3.igdb.com/games/' | jq '.[]' >> games.json
+
+printf "]\n}" >> games.json
+
+printf "{\n\"Genres\": [\n" > genres.json
+
+curl -H 'user-key: 401df87a02a8f4c13842a135bad415a4' \
+-d 'fields name, url;' \
+'https://api-v3.igdb.com/genres/' | jq '.[]' >> genres.json
+
+printf "]\n}" >> genres.json
+
