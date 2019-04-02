439d7b800e9745d2ce5e8c15c3c414175f1288d9
Madalyn Marabella <mmarabella@utexas.edu>
2019-04-02 18:11:44 -0500

    updates apiscript for companies

--- ApiScript.sh
+++ ApiScript.sh
@@ -2,19 +2,19 @@ rm -f companies.json
 rm -f games.json
 rm -f genres.json
 
-printf "{\n\"Companies\": [\n" > companies.json
+printf "{\n\"Companies\": " > companies.json
 
 curl -H 'user-key: 401df87a02a8f4c13842a135bad415a4' \
--d 'fields name, developed, description, logo; limit 50; \
-where developed != null & logo != null & description != null;' \
-'https://api-v3.igdb.com/companies/' | jq '.[]' >> companies.json
+-d 'fields name, developed, description, logo, country; limit 50; \
+where developed != null & logo != null & description != null & country != null;' \
+'https://api-v3.igdb.com/companies/' >> companies.json
 
-curl -H 'user-key: 401df87a02a8f4c13842a135bad415a4' \
--d 'fields name, developed, description, logo; offset 50; limit 50; \
-where developed != null & logo != null & description != null;' \
-'https://api-v3.igdb.com/companies/' | jq '.[]' >> companies.json
+#curl -H 'user-key: 401df87a02a8f4c13842a135bad415a4' \
+#-d 'fields name, developed, description, logo; offset 50; limit 50; \
+#where developed != null & logo != null & description != null;' \
+#'https://api-v3.igdb.com/companies/' | jq '.[]' >> companies.json
 
-printf "]\n}" >> companies.json
+printf "\n}" >> companies.json
 
 
 printf "{\n\"Games\": " > games.json
