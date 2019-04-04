a72c296bcb133c1ebeb2dd6a865ba1f23ca41a00
Alejandro Palacios <alex16palashus@gmail.com>
2019-04-04 00:47:57 -0500

    added sorting form to pillars

--- main.py
+++ main.py
@@ -6,15 +6,50 @@ import subprocess
 def index():
 	return render_template('index.html')
 
+
+
 @app.route('/games/')
 def games():
   field = request.args.get('field')
   direction = request.args.get('direction')
+
   if field == "name":
-    games = db.session.query(Game).join((Genre, Game.genres)).order_by(Game.name.asc()).all()
+    if direction == "desc":
+      games = db.session.query(Game).join((Genre, Game.genres)).order_by(Game.name.desc()).all()
+    else:
+      games = db.session.query(Game).join((Genre, Game.genres)).order_by(Game.name.asc()).all()
+
+  elif field == "summary":
+    if direction == "desc":
+      games = db.session.query(Game).join((Genre, Game.genres)).order_by(Game.summary.desc()).all()
+    else:
+      games = db.session.query(Game).join((Genre, Game.genres)).order_by(Game.summary.asc()).all()
+
+  elif field == "genre":
+    if direction == "desc":
+      games = db.session.query(Game).join((Genre, Game.genres)).order_by(Game.genre.desc()).all()
+    else:
+      games = db.session.query(Game).join((Genre, Game.genres)).order_by(Game.genre.asc()).all()
+
+  elif field == "company":
+    if direction == "desc":
+      games = db.session.query(Game).join((Genre, Game.genres)).order_by(Game.companies.desc()).all()
+    else:
+      games = db.session.query(Game).join((Genre, Game.genres)).order_by(Game.companies.asc()).all()
+
+  elif field == "rating":
+    if direction == "desc":
+      games = db.session.query(Game).join((Genre, Game.genres)).order_by(Game.rating.desc()).all()
+    else:
+      games = db.session.query(Game).join((Genre, Game.genres)).order_by(Game.rating.asc()).all()
+
   else:
     games = db.session.query(Game).join((Genre, Game.genres)).all()
+
   return render_template('games.html', games = games)
+
+
+
 
 @app.route('/genres/')
 def genres():
